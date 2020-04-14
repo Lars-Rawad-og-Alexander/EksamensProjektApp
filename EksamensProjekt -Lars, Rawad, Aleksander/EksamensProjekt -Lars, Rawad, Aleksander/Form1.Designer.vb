5341ba07dd8dddf005fc0976a09703d6831e581f
Lars Ole Rimmen <lars7891@elev.tradium.dk>
2020-04-14 08:42:48 +0200

    Lars, har lavet et treeview, hvor vi kan se nogle få emner.

--- EksamensProjekt -Lars, Rawad, Aleksander/EksamensProjekt -Lars, Rawad, Aleksander/Form1.Designer.vb
+++ EksamensProjekt -Lars, Rawad, Aleksander/EksamensProjekt -Lars, Rawad, Aleksander/Form1.Designer.vb
@@ -22,23 +22,60 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Sin,Cos,Tan")
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Trigonometri", New System.Windows.Forms.TreeNode() {TreeNode1})
+        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Firkanter areal")
+        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Firkanter", New System.Windows.Forms.TreeNode() {TreeNode3})
+        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Vektorer")
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(668, 156)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.Location = New System.Drawing.Point(84, 92)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(206, 202)
+        Me.Button1.Size = New System.Drawing.Size(277, 148)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(268, 453)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(132, 117)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.TreeView1.Location = New System.Drawing.Point(584, 102)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode1.Name = "Sin,Cos,Tan"
+        TreeNode1.Text = "Sin,Cos,Tan"
+        TreeNode2.Name = "Trigonometri"
+        TreeNode2.Text = "Trigonometri"
+        TreeNode3.Name = "Firkanter areal"
+        TreeNode3.Text = "Firkanter areal"
+        TreeNode4.Name = "Firkanter"
+        TreeNode4.Text = "Firkanter"
+        TreeNode5.Name = "Vektorer"
+        TreeNode5.Text = "Vektorer"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode2, TreeNode4, TreeNode5})
+        Me.TreeView1.Size = New System.Drawing.Size(262, 272)
+        Me.TreeView1.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1038, 594)
+        Me.ClientSize = New System.Drawing.Size(1904, 1041)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -47,4 +84,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TreeView1 As TreeView
 End Class
--- EksamensProjekt -Lars, Rawad, Aleksander/EksamensProjekt -Lars, Rawad, Aleksander/Form1.vb
+++ EksamensProjekt -Lars, Rawad, Aleksander/EksamensProjekt -Lars, Rawad, Aleksander/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
 
+    End Sub
 End Class
